118483dd8994771940222a03ca91cb3e5cda1461
BIGSOHN <ssm2327@gmail.com>
2023-10-30 18:31:07 +0900

    Modify Login, Join Function

--- ToDoList_dday/app/src/main/java/com/cookandroid/todolist/Join.kt
+++ ToDoList_dday/app/src/main/java/com/cookandroid/todolist/Join.kt
@@ -1,11 +1,65 @@
 package com.cookandroid.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.cookandroid.todolist.databinding.ActivityJoinBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Join : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private  lateinit var binding : ActivityJoinBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        auth = Firebase.auth
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_join)
+
+
+        // 뒤로 가기 버튼 클릭시 로그인 화면으로 이동
+        binding.backImg.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
+        val joinBtnClicked = findViewById<Button>(R.id.joinBtn)
+
+        joinBtnClicked.setOnClickListener {
+            val email = binding.idJoin
+            val pwd = binding.pwJoin
+            val pwd_check = binding.pwCheck
+
+            // 비밀번호 확인
+            if (pwd.text.toString() == pwd_check.text.toString()){
+                auth.createUserWithEmailAndPassword(
+                    email.text.toString(), pwd.text.toString()
+                )
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "회원가입 완료", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this, Login::class.java)
+                            startActivity(intent)
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(this, "회원가입 실패", Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+            }
+            else{
+                Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
     }
 }
--- ToDoList_dday/app/src/main/java/com/cookandroid/todolist/Login.kt
+++ ToDoList_dday/app/src/main/java/com/cookandroid/todolist/Login.kt
@@ -1,11 +1,53 @@
 package com.cookandroid.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.cookandroid.todolist.category.activity.MainActivity
+import com.cookandroid.todolist.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Login : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private  lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        auth = Firebase.auth
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_login)
+
+
+        // 가입하기 버튼 클릭시 수행
+        binding.joinText.setOnClickListener {
+            val intent = Intent(this, Join::class.java)
+            startActivity(intent)
+        }
+
+        // 로그인 버튼 클릭시 수행
+        binding.loginBtn.setOnClickListener {
+            val email = binding.emailInput
+            val pwd = binding.pwInput
+
+            auth.signInWithEmailAndPassword( email.text.toString(), pwd.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
+
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(this, "로그인 실패, 다시 시도해주세요.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
     }
 }
